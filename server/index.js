2f3f8301580c04a1b59eb6ac580870f77f4e50d7
Seifeldin Abouelella <69943178+seifeldingamal@users.noreply.github.com>
2021-06-16 04:16:08 +0200

    Added static forlder and prepared SPA

--- server/index.js
+++ server/index.js
@@ -12,6 +12,16 @@ const messages = require('./routes/api/messages');
 
 app.use('/api/messages', messages);
 
+
+// Handle production
+if(process.env.NODE_ENV === 'production') {
+    // Static folder
+    app.use(express.static(__dirname + '/public/'));
+
+    // Handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
